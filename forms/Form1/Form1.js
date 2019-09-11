9bc4d146640963492f4d3d61cf1546375a47b08c
Connor <cmg58665@gmail.com>
2019-09-11 17:16:46 -0500

    Initial commit

---
+++ forms/Form1/Form1.js
@@ -0,0 +1,29 @@
+/*
+Create a new appStudio project named Portfolio_TeamX.
+You will be creating a complete, text-based game named The Big Adventure. It is kind of like the angry birds activities you did, with the birds walking down paths. But here, your user is walking through rooms in a house and has to visit all of them to 'win'. The user has to move by giving the system input commands to move, read where the system tells her she is, and depend on descriptions of rooms (eg. to know if she has been in that room or not). All  things the user can't see. Basically think about playing Angry Birds with just a text interface. 
+
+Scenario: Your program lets users move through rooms in a ranch house (all one level) based on their input. Users can get descriptions of each room they are in. The goal is to visit all six rooms. You can configure house layout any way you like. 
+You will have to decide on the directions in which the user can move, how you will keep track of how far the user has moved within a room, which rooms she has visited and not visited, which room she is currently in, where in the room she is, etc.You'll have to output descriptions of this information for the user as she moves about since this is not a visual game. Also, you'll have to set the limits on how far she can move (ie. virtual walls around the room based on the size of the room). So if a user wants to move 5 units forward and that would take them outside of the room, the program needs to tell them something like they can't move further in that direction. You can assume that the user makes no errors in their input (eg. types in an 'a' when a number is expected by the program). 
+
+Example dialog might be: 
+Computer: Would you like to play The Big Adventure? 
+User: yes
+Computer: There are 6 rooms you need to visit: [here make up and display your six room names like Game Room, TV Room, Activity Room, Red Room, etc]. You must visit each of the six rooms at least once to win the game. The program will not stop until you win. 
+Computer: This is how you move: [give instructions on user input for movement] ......
+Computer: Start now by giving me your first move. 
+User: etc. .......
+
+Hint: Start this Fixit by writing a rough algorithm (list of steps) of how the game will work. Then use it to pretend to play the game (without the computer). Get your ideas in place and working before you start coding. Doing it this way will seem to take longer since it delays coding, but in reality your coding goes muck faster and has fewer errors when you make a detailed plan first. 
+
+Requirements
+Use six rooms, each with its' own name and description
+You must use at least one array, prompt, alert (talk to the user via alerts, to developers like yourselves with console.logs), if/else, switch, and template literal.
+You will likely also use confirms, booleans, single variables.
+Use a comment above any code blocks/units that are not entirely clear to explain in plain (and concise) English what they do.
+Do not use any loops or functions (we haven't learned these yet). Just use code that we have learned to date.
+On the Word doc, detail exactly what each user did for the portfolio (working individually, in pairs, etc is fin -just document). I will use this along with my observations and questions for you in class to adjust individual grades on this portfolio up or down.
+Hand In: Submit one zipped team project.
+
+
+
+*/
